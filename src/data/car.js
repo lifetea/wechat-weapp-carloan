2a5ae619b78c7aa5f0190a80221b75e499128e38
lifetea <494886251@qq.com>
2018-06-11 17:00:34 +0800

    fixs #1 init

---
+++ src/data/car.js
@@ -0,0 +1,53 @@
+const list = [
+  {
+    id: 315,
+    image: '../images/golf.png',
+    name: '新高尔夫 1.2T舒适型',
+    firstPay: 9040,
+    monthPay: 4565.89,
+    price: 150800,
+    desc: '极地白+黑'
+
+  },
+  {
+    id: 316,
+    image: '../images/sagitar.png',
+    name: '新速腾 1.6T自动舒适',
+    firstPay: 12888,
+    monthPay: 2416,
+    price: 150800,
+    desc: '外观珍珠白 + 内饰米咖色'
+  },
+  {
+    id: 317,
+    image: '../images/trek.png',
+    name: '1.4T自动舒适型+氙灯',
+    firstPay: 9040,
+    monthPay: 4593.14,
+    price: 151700,
+    desc: '极地白+黑'
+
+  },
+  {
+    id: 318,
+    image: '../images/sportsvan.png',
+    name: '哈瓦那橙+黑',
+    firstPay: 9845,
+    monthPay: 5053.36,
+    price: 166900,
+    desc: '极地白+黑'
+
+  },
+  {
+    id: 920,
+    image: '../images/sportsvan.png',
+    name: '哈瓦那橙+黑',
+    firstPay: 9845,
+    monthPay: 5053.36,
+    price: 166900,
+    desc: '极地白+黑'
+
+  }
+]
+
+export default list
